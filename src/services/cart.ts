996d1dae0f22677645cfc2e525e4a4eeaadcb6ee
TamamTamar <tamar.tamam770@gmail.com>
2024-10-15 22:10:29 +0300

    dd

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = "https://nodestudio.onrender.com/api/v1/";
+const baseUrl = "https://nodestudio.onrender.com/api/v1";
 const cartUrl = `${baseUrl}/cart`;
 
 export const getCart = () => {
