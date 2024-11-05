6a4131f55e6281d86bb61bfb12c907aedf406f64
TamamTamar <tamar.tamam770@gmail.com>
2024-11-05 14:27:45 +0200

    id

--- src/routes/LandingPageHebrew/LandingPageHebrew.tsx
+++ src/routes/LandingPageHebrew/LandingPageHebrew.tsx
@@ -12,44 +12,44 @@ const LandingPageHeb = () => {
         <div className="landing-page-heb">
 
             <MainBanner />
-            <OurServiceN/>
+            <OurServiceN />
             <div>
-                <h2 className='project-title'>הפרוייקטים שלנו</h2>
-            <BannerSection
-            sideClass="right"
-                href="https://finalproject-ousr.onrender.com/about"
-                title="Full Stack Development"
-                name="T&T Fashion"
-                description="חנות אונליין שנבנתה עם React ו-NodeJS"
-                sectionClass="fashion-heb"
-            />
-            <BannerSection
-                sideClass="left"
-                href="http://www.alex-osadchi.com/primo/"
-                title="Software Design"
-                name="Priimo"
-                description="התאמת טיפולים אישיים לסרטן"
-                sectionClass="priimo-heb"
-            />
-            <BannerSection
-                sideClass="right"
-                href="http://www.alex-osadchi.com/ilaan-shemesh/"
-                title="Management System"
-                name="Ilan Overview"
-                description="אפליקציה לניהול ליקויי דירה ומסירת דירות לרוכשים"
-                sectionClass="shemesh-heb"
-            />
-            <BannerSection
-                sideClass="left"
-                href="http://www.alex-osadchi.com/reability-online-2/"
-                title="Video Motion Game"
-                name="ReAbility Online"
-                description="משחקים לשיפור הקוגנטיביות"
-                sectionClass="reability-heb"
-            />
-        </div>
+                <h2 id="firstProject" className='project-title'>הפרוייקטים שלנו</h2>
+                <BannerSection
+                    sideClass="right"
+                    href="https://finalproject-ousr.onrender.com/about"
+                    title="Full Stack Development"
+                    name="T&T Fashion"
+                    description="חנות אונליין שנבנתה עם React ו-NodeJS"
+                    sectionClass="fashion-heb"
+                />
+                <BannerSection
+                    sideClass="left"
+                    href="http://www.alex-osadchi.com/primo/"
+                    title="Software Design"
+                    name="Priimo"
+                    description="התאמת טיפולים אישיים לסרטן"
+                    sectionClass="priimo-heb"
+                />
+                <BannerSection
+                    sideClass="right"
+                    href="http://www.alex-osadchi.com/ilaan-shemesh/"
+                    title="Management System"
+                    name="Ilan Overview"
+                    description="אפליקציה לניהול ליקויי דירה ומסירת דירות לרוכשים"
+                    sectionClass="shemesh-heb"
+                />
+                <BannerSection
+                    sideClass="left"
+                    href="http://www.alex-osadchi.com/reability-online-2/"
+                    title="Video Motion Game"
+                    name="ReAbility Online"
+                    description="משחקים לשיפור הקוגנטיביות"
+                    sectionClass="reability-heb"
+                />
+            </div>
 
-           
+
             <AboutUs />
 
             <div id="contactForm">
