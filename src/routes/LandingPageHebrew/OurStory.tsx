a97a6fca1bab3594befb55b977b94beadd9fec49
TamamTamar <tamar.tamam770@gmail.com>
2024-12-05 20:18:00 +0200

    nm

--- src/routes/LandingPageHebrew/OurStory.tsx
+++ src/routes/LandingPageHebrew/OurStory.tsx
@@ -35,7 +35,7 @@ const OurStory = () => {
                 <div className="step-right-side">
                     <div className="step-card right num2">
                         <span className="image-circle">
-                            <img src="img/compass.svg" alt="compass" />
+                            <img className='icon' src="img/compass.svg" alt="compass" />
                         </span>
                         <h3>פיתוח אתרים וחנויות בקוד פתוח (Full Stack)</h3>
                         <p>
@@ -46,7 +46,7 @@ const OurStory = () => {
                     </div>
                     <div className="step-card right num4">
                         <span className="image-circle">
-                            <img src="img/rocket-02.svg" alt="" />
+                            <img className='icon' src="img/rocket-02.svg" alt="" />
                         </span>
                         <h3>פיתוח אסטרטגיה שיווקית</h3>
                         <p>
@@ -58,7 +58,7 @@ const OurStory = () => {
                 </div>
 
                 <div className="step-center">
-                    <img src="img/line.svg" alt="line" />
+                    <img className='line' src="img/line.svg" alt="line" />
                     <p
                         className="number num1"
                         ref={(el) => (numbersRef.current[0] = el)}
@@ -88,7 +88,7 @@ const OurStory = () => {
                 <div className="step-left-side">
                     <div className="step-card left num1">
                         <span className="image-circle">
-                            <img src="img/face-happy.svg" alt="compass" />
+                            <img className='icon' src="img/face-happy.svg" alt="compass" />
                         </span>
                         <h3>היכרות מעמיקה</h3>
                         <p>
@@ -99,14 +99,10 @@ const OurStory = () => {
                     </div>
                     <div className="step-card left num3">
                         <span className="image-circle">
-                            <img src="img/Frame 6481.svg" alt="" />
+                            <img className='icon' src="img/Frame 6481.svg" alt="" />
                         </span>
-                        <h3>פיתוח אתרים וחנויות בקוד פתוח (Full Stack)</h3>
-                        <p>
-                            נאפיין במדויק את הצרכים והפונקציונליות של האתר, עם התאמה
-                            טכנית מושלמת. נתמקד בחוויית משתמש (UX) ובממשק משתמש (UI)
-                            שיעניקו למבקרים חוויה יוצאת דופן.
-                        </p>
+                      <h3>אפיון ועיצוב חוויית משתמש (UX/UI)</h3>
+                   <p>נעבוד בצמוד כדי ליצור מוצר שמשלב יופי וטכנולוגיה. אנחנו נשלב את היכולות שלנו בעיצוב ופיתוח כדי להביא את הרעיון שלך לחיים. האתר יהיה מהיר, מאובטח ונגיש, עם קוד נקי ועיצוב מרהיב.</p>
                     </div>
                 </div>
             </div>
